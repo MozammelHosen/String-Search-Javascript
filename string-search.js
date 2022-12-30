98202ba427d37714c2f76c96ceb3677a39b71439
MozammelHosen <mdmozammelhosen15701@gmail.com>
2022-12-30 20:50:15 +0600

    Search All String Deceliyar

--- string-search.js
+++ string-search.js
@@ -1,22 +1,54 @@
 const products = [
-  "Hp EliteBook laptop",
+  "Hp EliteBook Laptop",
   "HTC Laptop",
   "Walton Laptop",
   "Hp EliteBook Rifle",
   "Samsung laptop",
-  "laptop"
+  "Laptop",
 ];
 const searching = "Laptop";
-// IndexOf 
+// const searching = "hp";
+// IndexOf
 const output = [];
 // For in loop for searching
-for (const product of products) {
-    if(product.toLowerCase().indexOf(searching.toLowerCase()) != -1) {
-        output.push(product);
-    }
-    else{
-        console.log("Not Found");
-    }
+// for (const product of products) {
+//   if (product.toLowerCase().indexOf(searching.toLowerCase()) != -1) {
+//     output.push(product);
+//   } else {
+//     console.log("Not Found");
+//   }
+// }
+// console.log(output);
 
+// Declier To includes
+// for (const product of products) {
+//   if (product.toLowerCase().includes(searching.toLowerCase())) {
+//     output.push(product);
+//   } else {
+//     console.log("Not Found");
+//   }
+// }
+// console.log(output);
+
+// Search-StartWith
+
+// for (const product of products) {
+//   if (product.toLowerCase().startsWith(searching.toLowerCase())) {
+//     output.push(product);
+//   } else {
+//     console.log("Page Not Found");
+//   }
+// }
+// console.log(output);
+
+// Ends With
+
+for (const product of products) {
+  if (product.toLowerCase().endsWith(searching.toLowerCase())) {
+    output.push(product);
+  } else {
+    console.log("Ends With Page Not Found");
+  }
 }
+
 console.log(output);
